1d8802bdfe107feb91caa810198e0ac32abe0fa4
ajcastan2001 <alexander.castaneda@gmail.com>
2023-11-19 02:23:06 -0500

    added

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const characterRoutes = require('./src/characters/routes');
+require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000; 
 
 app.use(express.json());
 
-app.get('/', (req,res) =>{
+app.get('/', (req, res) => {
     res.send('Honkai Star Rail API');
-
 });
 
-app.use('/api/v1/characters',characterRoutes);
+app.use('/api/v1/characters', characterRoutes);
 
-
-app.listen(PORT, () => {console.log(`Server running on PORT ${PORT}`)});
+app.listen(PORT, () => {
+    console.log(`Server running on PORT ${PORT}`);
+});
