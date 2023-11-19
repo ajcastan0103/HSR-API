00fb4ae50d4ea764f8a8a2fc44b64ecd24aed8b9
ajcastan2001 <alexander.castaneda@gmail.com>
2023-11-19 03:31:49 -0500

    added

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000; 
 
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
     res.send('Honkai Star Rail API');
